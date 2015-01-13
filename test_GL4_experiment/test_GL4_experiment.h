03e22dca144e1562dd0d19940634e3957bd504a7
Gilbert Wong <djbuzzkill@gmail.com>
2015-01-12 21:53:07 -0800

    more stuff
    merging stuff

---
+++ test_GL4_experiment/test_GL4_experiment.h
@@ -0,0 +1,36 @@
+
+
+#ifndef _TEST_GL4_EXPERIMENT_H_
+#define _TEST_GL4_EXPERIMENT_H_
+
+#include <map>
+#include <list>
+#include <vector>
+#include <string>
+#include <memory>
+#include <sstream>
+#include <algorithm>
+
+#include <Dx/System.h>
+#include <OpenGL_system/OpenGL_system.h>
+//#include <Dx/IKSolver2d.h>
+//#include <Xp/Xp.h> 
+//#include <Dx/Horde3D_platform.h> 
+//
+//#include <Horde3D.h>
+//#include <Horde3DUtils.h>
+
+
+#include <SDL.h>
+#include <GL/glew.h>
+#include <SDL2_platform\SDL2_platform.h>
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+
+
+#include <gli/gli.hpp>
+// #include <il/il.h>
+
+#endif
