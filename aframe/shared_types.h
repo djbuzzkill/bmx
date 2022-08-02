b7934430b36be2b47b12ce4373980f7e7a3dd32e
Gilbert <djbuzzkill@gmail.com>
2022-08-01 19:47:57 -0600

    and this

---
+++ aframe/shared_types.h
@@ -0,0 +1,28 @@
+#ifndef AF_SHARED_TYPES_H
+#define AF_SHARED_TYPES_H
+
+
+
+#include "common.h"
+
+
+namespace af
+{
+
+  
+  typedef std::vector<unsigned char>         bytearray; 
+  
+  typedef std::array<unsigned char, 32>      byte32; // 
+  typedef std::array<unsigned char, 20>      byte20; // mainly for ripemd160
+
+
+  typedef byte32 bigint32;
+  typedef byte32 fixnum32; 
+ 
+}
+
+
+
+
+
+#endif
