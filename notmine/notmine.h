3e91f0782e2b804aebef91532766a959cb45cb97
Gilbert <djbuzzkill@gmail.com>
2022-09-28 11:44:43 -0600

    wait

---
+++ notmine/notmine.h
@@ -0,0 +1,23 @@
+#ifndef NOTMINE_H
+#define NOTMINE_H
+
+#include <cstdio>
+#include <cstring>
+
+#include <map>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <array>
+
+
+
+#include "zmq.h"
+#include "aframe/af.h"
+#include "bmx/bmx.h"
+
+
+
+
+#endif
