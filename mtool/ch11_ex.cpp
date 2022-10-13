b686d585ae6cb68ab43b78b3b222d6df1be6e188
Gilbert <djbuzzkill@gmail.com>
2022-10-12 20:36:32 -0600

    excercise

---
+++ mtool/ch11_ex.cpp
@@ -0,0 +1,21 @@
+
+
+
+int test_merkle_tree_populate_1(const std::vector<std::string>& args) ;
+int test_merkle_tree_populate_2(const std::vector<std::string>& args) ;
+int test_merkle_block_parse    (const std::vector<std::string>& args) ;
+int test_valid_merkle_block    (const std::vector<std::string>& args) ;
+
+
+//
+//
+int CH11_Ex (const std::vector<std::string>& args) {
+  FN_SCOPE ();
+
+  test_merkle_tree_populate_1(args) ;
+  test_merkle_tree_populate_2(args) ;
+  test_merkle_block_parse    (args) ;
+  test_valid_merkle_block    (args) ;
+
+  return 0;
+}
---
+++ mtool/ch12_ex.cpp
@@ -0,0 +1,10 @@
+
+
+//
+//
+int CH12_Ex (const std::vector<std::string>& args) {
+  FN_SCOPE ();
+
+
+  return 0;
+}
